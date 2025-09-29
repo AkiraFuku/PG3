8be8dfec4b6ac9012c3f98b51343e49f1e175b47
AkiraFuku <k024g1002@g.neec.ac.jp>
2025-09-29 12:58:29 +0900

    01_01　駅名リスト

--- 01_01/main.cpp
+++ 01_01/main.cpp
@@ -6,18 +6,41 @@ using namespace std;
 
 
 void Draw(list<const char*> stations) {
+	int num = 1;
 	for (const char* station : stations) {
-		printf("|Station: %s|\n", station);
+
+		
+		printf("|%02d|Station: %s|\n",num, station);
+		num++;
 	}
 }
-int main() {
-	list<const char*> stations2022{ "Tokyo", "Kanda", "Akihabara","Okachimchi","Ueno","Uguisudani","Nippori","Nisi-Nippori","Tabata","Komagome","Sugamo","Otuka","Ikebukuro","Mejiro","Takadanobaba,","Shin-Okubo","shinjuku","Yoyogi","Harajuku","Shibuya","Ebisu","Meguro","Gotanda","Osaki","Shinagawa","Tamachi","Hamamatucho","Shinbashi",};
-	list<const char*> stations2019{ "Station D", "Station E", "Station F" };
-	list<const char*> stations1970{ "Station G", "Station H", "Station I" };
+/// <summary>
+/// 指定した位置に駅名をリストに挿入します。
+/// </summary>
+/// <param name="stations">駅名（const char*）のリストへの参照。挿入先となります。</param>
+/// <param name="stationName">挿入する駅名（const char*）。</param>
+/// <param name="pos">挿入位置を示すインデックス（0始まり）。</param>
+void Input(list<const char*>& stations,const char* stationName,int pos) {
 
-	Draw(stations2022);
-	Draw(stations2019);
-	Draw(stations1970);
+	
+	auto it = stations.begin();
+	for (int i = 0; i < pos-1 && it != stations.end(); i++, it++);
+	stations.insert(it, stationName);
+
+	
+}
+
+int main() {
+	list<const char*> stations{ "Tokyo", "Kanda", "Akihabara","Okachimchi","Ueno","Uguisudani","Nippori","Tabata","Komagome","Sugamo","Otuka","Ikebukuro","Mejiro","Takadanobaba,","Shin-Okubo","shinjuku","Yoyogi","Harajuku","Shibuya","Ebisu","Meguro","Gotanda","Osaki","Shinagawa","Tamachi","Hamamatucho","Shimbashi","Yurakucho"};
+	
+	printf("1970\n");
+	Draw(stations);
+	Input(stations,"Nisi-Nippori", 8);
+	printf("2019\n");
+	Draw(stations);
+	Input(stations, "Takanawa Gatway", 26);
+	printf("2022\n");
+	Draw(stations);
 
 	return 0;
 }
