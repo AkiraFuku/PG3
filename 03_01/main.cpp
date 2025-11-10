b3fc4ac832a1572c6fc67ed900f524c6ba22e919
AkiraFuku <k024g1002@g.neec.ac.jp>
2025-11-10 12:57:41 +0900

    03_01完了

--- 03_01/main.cpp
+++ 03_01/main.cpp
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <Windows.h>
 #include <fcntl.h>
 #include <io.h>
-
 #include "Enemy.h"
+
+
+
 
 int main(int argc,const char * argv[]){
 	int modeResult = _setmode(_fileno(stdout), _O_U16TEXT);
@@ -10,22 +13,12 @@ int main(int argc,const char * argv[]){
 		fwprintf(stderr, L"_setmode に失敗しました。\n");
 		return 1;
 	}
-	
-	 modeResult = _setmode(_fileno(stdin),  _O_U16TEXT);
-	if (modeResult == -1) {
-		fwprintf(stderr, L"_setmode に失敗しました。\n");
-		return 1;
-	}
-
-
 	Enemy enemy;
 	enemy.initialize();
-	for (int i = 0; i < 5; ++i)
+	for (int i = 0; i < 3; ++i)
 	{
 		enemy.update();
 	}
-
-
-	
 	return 0;
 }
+
