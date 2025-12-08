66b65545517afc765850072ccd93cde90e9a72d7
AkiraFuku <k024g1002@g.neec.ac.jp>
2025-12-08 10:24:52 +0900

    Title

---
+++ 04_01/TitleScene.cpp
@@ -0,0 +1,16 @@
+#include "TitleScene.h"
+void TitleScene::Init() {
+    sceneNo_ = TITLE; // 自分はタイトル
+    titleGraph_ = Novice::LoadTexture("./Resources/images/RRtitle.png");
+}
+
+void TitleScene::Update(char* keys, char* preKeys) {
+    // スペースキーでステージへ
+    if (keys[DIK_SPACE] && !preKeys[DIK_SPACE]) {
+        sceneNo_ = STAGE; 
+    }
+}
+
+void TitleScene::Draw() {
+    Novice::DrawSprite(0, 0, titleGraph_, 1.0f, 1.0f, 0.0f, 0xFFFFFFFF);
+}
