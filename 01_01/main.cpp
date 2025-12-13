ff2a9cb9141ab61cb9cc65613a70a63667818dfc
AkiraFuku <k024g1002@g.neec.ac.jp>
2025-12-13 11:41:30 +0900

    カンマ区切りスペース

--- 01_01/main.cpp
+++ 01_01/main.cpp
@@ -9,8 +9,8 @@ void Draw(list<const char*> stations) {
 	int num = 1;
 	for (const char* station : stations) {
 
-		
-		printf("|%02d|Station: %s|\n",num, station);
+
+		printf("|%02d|Station: %s|\n", num, station);
 		num++;
 	}
 }
@@ -20,22 +20,22 @@ void Draw(list<const char*> stations) {
 /// <param name="stations">駅名（const char*）のリストへの参照。挿入先となります。</param>
 /// <param name="stationName">挿入する駅名（const char*）。</param>
 /// <param name="pos">挿入位置を示すインデックス（0始まり）。</param>
-void Input(list<const char*>& stations,const char* stationName,int pos) {
+void Input(list<const char*>& stations, const char* stationName, int pos) {
 
-	
+
 	auto it = stations.begin();
-	for (int i = 0; i < pos-1 && it != stations.end(); i++, it++);
+	for (int i = 0; i < pos - 1 && it != stations.end(); i++, it++);
 	stations.insert(it, stationName);
 
-	
+
 }
 
 int main() {
-	list<const char*> stations{ "Tokyo", "Kanda", "Akihabara","Okachimchi","Ueno","Uguisudani","Nippori","Tabata","Komagome","Sugamo","Otuka","Ikebukuro","Mejiro","Takadanobaba,","Shin-Okubo","shinjuku","Yoyogi","Harajuku","Shibuya","Ebisu","Meguro","Gotanda","Osaki","Shinagawa","Tamachi","Hamamatucho","Shimbashi","Yurakucho"};
-	
+	list<const char*> stations{ "Tokyo", "Kanda", "Akihabara", "Okachimchi", "Ueno", "Uguisudani" ,"Nippori", "Tabata", "Komagome", "Sugamo", "Otuka", "Ikebukuro", "Mejiro", "Takadanobaba,", "Shin-Okubo", "shinjuku", "Yoyogi", "Harajuku", "Shibuya", "Ebisu", "Meguro", "Gotanda", "Osaki", "Shinagawa", "Tamachi", "Hamamatucho", "Shimbashi", "Yurakucho" };
+
 	printf("1970\n");
 	Draw(stations);
-	Input(stations,"Nisi-Nippori", 8);
+	Input(stations, "Nisi-Nippori", 8);
 	printf("2019\n");
 	Draw(stations);
 	Input(stations, "Takanawa Gatway", 26);
