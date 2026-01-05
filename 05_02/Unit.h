96661d4d7a9c4d7b36b5e506751d39b65316a2a6
AkiraFuku <k024g1002@g.neec.ac.jp>
2026-01-05 11:00:23 +0900

    unit

---
+++ 05_02/Unit.h
@@ -0,0 +1,20 @@
+#pragma once
+class Selector;
+class Unit
+{
+private:
+	int mapX_=0;
+	int mapY_=0;
+	int mapSize_=32;
+	int id=0;
+	bool IsSelected_=false;
+
+public:
+	Unit()=default;
+	void Init(int idref);
+	void Update();
+	void Draw();
+	void Move(int x,int y);
+	void MoveEnd(Selector* selector);
+};
+
