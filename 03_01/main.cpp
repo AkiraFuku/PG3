4dbe329aa28dcf69e7c0cdb33ca5c7e64d7946ba
AkiraFuku <k024g1002@g.neec.ac.jp>
2026-02-09 11:03:51 +0900

    ファイル

--- 03_01/main.cpp
+++ 03_01/main.cpp
@@ -8,11 +8,11 @@
 
 
 int main(int argc,const char * argv[]){
-	int modeResult = _setmode(_fileno(stdout), _O_U16TEXT);
+	/*int modeResult = _setmode(_fileno(stdout), _O_U16TEXT);
 	if (modeResult == -1) {
 		fwprintf(stderr, L"_setmode に失敗しました。\n");
 		return 1;
-	}
+	}*/
 	Enemy enemy;
 	enemy.initialize();
 	for (int i = 0; i < 3; ++i)
